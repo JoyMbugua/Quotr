a7c7089e63a8748d891e028e7f5ef5ec1c9f0c9d
JoyMbugua <wacumbugua@gmail.com>
2021-01-31 15:33:49 +0300

    added the like and dislike logic

--- src/app/votes/votes.component.ts
+++ src/app/votes/votes.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VotesComponent implements OnInit {
 
+  upVoteCount: number = 0;
+  downVoteCount: number = 0;
+
+  upVote(){
+    this.upVoteCount++;
+  }
+  downVote(){
+    this.downVoteCount++;
+  }
   constructor() { }
 
   ngOnInit(): void {
